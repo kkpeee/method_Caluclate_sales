3874a90592dd493f6b70df39828b22df6e6b11f5
kkpeee <rin.y8b@gmail.com>
2016-05-10 17:29:16 +0900

    Add files via upload

--- jp/co/iccom/higashi_takahiro/calculate_sales/Calculatesales.java
+++ jp/co/iccom/higashi_takahiro/calculate_sales/Calculatesales.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -15,7 +14,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class Calculatesales {
-	public static void main(String[]args) throws IOException{
+	public static void main(String[]args) throws Exception{
 		if(args.length == 0 || args.length > 1  || !new File(args[0]).exists()) {
 			System.out.println("予期せぬエラーが発生しました");
 			return;
@@ -62,7 +61,7 @@ public class Calculatesales {
 		for(int i = 0; i < rcdFolder.size(); i++) {
 			if(!readRcd(folder,rcdFolder.get(i), rcdEarings )){
 				return;
-    	}
+			}
 			String branchCode = rcdEarings.get(0);
 			String commodityCode = rcdEarings.get(1);
 			long amount = Long.parseLong(rcdEarings.get(2));
@@ -95,8 +94,8 @@ public class Calculatesales {
 	}
 	
 	//「メソッド」定義ファイル読み込み、存在判定、フォーマットチェック
-	private static  boolean readTeigiFile(String branchOrCommodity,File fileName,String format,
-			HashMap<String, String> branchOrCommodityMap,HashMap<String, Long> branchOrCommodityRcdMap) {
+	private static  boolean readTeigiFile(String definitionFile,File fileName,String format,
+			HashMap<String, String> definitionFileMap,HashMap<String, Long> definitionFileRcdMap) {
 		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(fileName));
@@ -104,23 +103,23 @@ public class Calculatesales {
 			while ((line = br.readLine()) != null) {
 				String[] words =line.split(",");
 				if (words.length != 2 || !words[0].matches(format)) {
-					System.out.println(branchOrCommodity + "定義ファイルのフォーマットが不正です");
+					System.out.println(definitionFile + "定義ファイルのフォーマットが不正です");
 					return false;
 				}
-				branchOrCommodityMap.put(words[0],words[1]);// [0]支店コード　[1]支店名
-				branchOrCommodityRcdMap.put(words[0],0L);
+				definitionFileMap.put(words[0],words[1]);// [0]支店コード　[1]支店名
+				definitionFileRcdMap.put(words[0],0L);
 			}
-		} catch (IOException e) {
-				System.out.println(branchOrCommodity + "定義ファイルが存在しません");
+		} catch (Exception e) {
+				System.out.println(definitionFile + "定義ファイルが存在しません");
 				return false;
-			} finally {
+		} finally {
 				try {
 					br.close();
-				} catch (IOException e) {
+				} catch (Exception e) {
 					System.out.println("予期せぬエラーが発生しました");
 					return false;
 				}
-			}
+		}
 		return true;
 	}
 
@@ -132,7 +131,7 @@ public class Calculatesales {
 			String readLine;
 			while((readLine = br.readLine()) != null){
 				rcdEarings.add(readLine); //get(0)001,get(1)SFT0001,get(2)10000
-				}	
+			}	
 			// 売り上げファイルの中身が４行以上ある場合フォーマットエラー
 			if((rcdEarings.size() != 3)) {
 				System.out.println(fileName + "のフォーマットが不正です");
@@ -144,7 +143,7 @@ public class Calculatesales {
 		}finally{
 			try {
 				br.close();
-				} catch (IOException e) {
+				} catch (Exception e) {
 					System.out.println("予期せぬエラーが発生しました");
 					return false;
 				}
@@ -153,12 +152,12 @@ public class Calculatesales {
 	}
 
     //「メソッド」売上ファイル集計、コードエラー処理
-	private static  boolean amountCheck(String branchOrCommodity, String fileName,long amount,
+	private static  boolean amountCheck(String definitionFile, String fileName,long amount,
 			HashMap<String, Long> amountRcdMap, String code) {
 		
 		// 定義ファイル、コードエラー処理
 		if (!amountRcdMap.containsKey(code)) {
-			System.out.println(fileName + "の"+ branchOrCommodity + "コードが不正です");
+			System.out.println(fileName + "の"+ definitionFile + "コードが不正です");
 			return false;
 		}
 				
@@ -178,24 +177,24 @@ public class Calculatesales {
 
 //「メソッド」商品別集計ファイルの作成
 
-	private static boolean writeRcd(File fileName, List<Map.Entry<String, Long>> sortBranchOrCommodity, 
+	private static boolean writeRcd(File fileName, List<Map.Entry<String, Long>> sortdefinitionFile, 
 			HashMap<String, String> branchOrCommdityMap){
 		BufferedWriter bw = null;
 		try {
 			bw = new BufferedWriter(new FileWriter(fileName));
 			fileName.createNewFile();
 			
-			for (Entry<String, Long> e : sortBranchOrCommodity) {
+			for (Entry<String, Long> e : sortdefinitionFile) {
 				bw.write(e.getKey() + "," + branchOrCommdityMap.get(e.getKey()) +","+ e.getValue());
 				bw.newLine();
 			}
-		} catch (IOException e) {
+		} catch (Exception e) {
 			System.out.println("予期せぬエラーが発生しました");
 			return false;
 		} finally {
 			try {
 				bw.close();
-			} catch (IOException e) {
+			} catch (Exception e) {
 				System.out.println("予期せぬエラーが発生しました");
 				return false;
 			}	
@@ -204,20 +203,18 @@ public class Calculatesales {
 		return true;
 	}
 	
-	//「メソッド」ソート　降順並べ替え
-	
 //「メソッド」ソート降順
-	private static List<Entry<String,Long>> sortRcd(Map<String, Long> branchOrCommodityRcdMap) {
-	List<Map.Entry<String, Long>> sortBranchOrCommodity 
-	= new ArrayList<Map.Entry<String,Long>>(branchOrCommodityRcdMap.entrySet());
-	Collections.sort(sortBranchOrCommodity, new Comparator<Map.Entry<String,Long>>() {
+	private static List<Entry<String,Long>> sortRcd(Map<String, Long> definitionFileRcdMap) {
+	List<Map.Entry<String, Long>> sortdefinitionFile 
+	= new ArrayList<Map.Entry<String,Long>>(definitionFileRcdMap.entrySet());
+	Collections.sort(sortdefinitionFile, new Comparator<Map.Entry<String,Long>>() {
 		@Override
 		public int compare(Entry<String,Long> entry1, Entry<String,Long> entry2) {
 			return (entry2.getValue()).compareTo(entry1.getValue());
-			}
+		}
 	});
-	return sortBranchOrCommodity;
-}
+	return sortdefinitionFile;
+	}
 	
 
 	//「メソッド」連番処理
@@ -249,5 +246,5 @@ public class Calculatesales {
 			}
 		}
 		return;
-		}
+	}
 }
